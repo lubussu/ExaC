5bc404ed793782168c14c2c8fdfabe5ab39129f3
lcaprioli96 <93045436+lcaprioli17@users.noreply.github.com>
2022-06-12 15:46:33 +0200

    Dataset revisionati

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('./dataset/allexoplanets.csv', low_memory=False)
+df = pd.read_csv('./dataset/All Confirmed Exoplanets.csv', low_memory=False)
 print(df.columns)
 
