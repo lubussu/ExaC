6522e71a6247cd9c76c5f46352dd0bcc33cb158d
lcaprioli96 <93045436+lcaprioli17@users.noreply.github.com>
2022-06-16 11:28:28 +0200

    rimossi attributi descrittivi da tess

--- pp_tess.py
+++ pp_tess.py
@@ -13,19 +13,24 @@ def pre_process():
         inplace=True)
 
     print("tess prev attributes:" + str(tess.columns.size))
-    thresh = len(tess) * .7
-    tess.dropna(thresh=thresh, axis=1, inplace=True) #remove columns with less than 70% of not nan values
 
-    nunique = tess.nunique() #series with numner of unique value for each column
-    cols_to_drop = nunique[nunique == 1].index #indexes of columns with value of nunique == 1
+    tess.drop(
+        columns=['toipfx', 'ctoi_alias', 'pl_pnum', 'rastr', 'decstr',
+                 'toi_created', 'rowupdate'],
+        inplace=True)
+
+    thresh = len(tess) * .7
+    tess.dropna(thresh=thresh, axis=1, inplace=True)  # remove columns with less than 70% of not nan values
+
+    nunique = tess.nunique()  # series with numner of unique value for each column
+    cols_to_drop = nunique[nunique == 1].index  # indexes of columns with value of nunique == 1
     tess.drop(cols_to_drop, axis=1, inplace=True)
 
-    tess.drop_duplicates(subset='toi', keep='first') #remove rows with duplicate value of kepoi_name column
+    tess.drop_duplicates(subset='toi', keep='first')  # remove rows with duplicate value of kepoi_name column
 
-    tess = tess[tess.columns.drop(list(tess.filter(regex='err')))] #remove columns that contains err in attribute name
-    tess = tess[tess.columns.drop(list(tess.filter(regex='lim')))] #remove columns that contains lim in attribute name
+    tess = tess[tess.columns.drop(list(tess.filter(regex='err')))]  # remove columns that contains err in attribute name
+    tess = tess[tess.columns.drop(list(tess.filter(regex='lim')))]  # remove columns that contains lim in attribute name
 
     print("tess next attributes:" + str(tess.columns.size))
 
-
-    #tess.to_csv('./dataset/tess.csv')
+    # tess.to_csv('./dataset/tess.csv')
