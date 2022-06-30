e900bc0c543df1b803a7ab2d20f43fe9fddd3226
lcaprioli96 <93045436+lcaprioli17@users.noreply.github.com>
2022-06-30 17:46:58 +0200

    script per estrazione delle features dalle light curves

--- src/lightcurves_features.py
+++ src/lightcurves_features.py
@@ -1,6 +1,7 @@
 import math
 
 import numpy as np
+import scipy.stats
 import sklearn as sk
 from astropy.io import fits
 from astropy.table import Table
@@ -34,20 +35,45 @@ with fits.open(filename, mode="readonly") as hdulist:
     sap_fluxes = hdulist[1].data['SAP_FLUX']
     pdcsap_fluxes = hdulist[1].data['PDCSAP_FLUX']
 
-    a = pd.Series(pdcsap_fluxes)
-    a.interpolate(inplace=True)
+    lc = pd.Series(pdcsap_fluxes)
+    a = lc.interpolate()
 
     # print(len(times))
     # print(len(sap_fluxes))
-    print(len(pdcsap_fluxes))
+    # print(len(pdcsap_fluxes))
+    print(np.isnan(times).sum())
     print(np.isnan(a).sum())
-    print(np.max(a))
-    print(np.min(a))
-    print(np.mean(a))
-    print(np.quantile(a, 0.25))
-    print(np.quantile(a, 0.5))
-    print(np.quantile(a, 0.75))
-    print(np.std(a))
+
+    lmax = np.max(a)
+
+    lmin = np.min(a)
+
+    mean = np.mean(a)
+
+    q1 = np.quantile(a, 0.25)
+
+    median = np.quantile(a, 0.5)
+
+    q3 = np.quantile(a, 0.75)
+
+    std = np.std(a)
+
+    amp = (lmax - lmin)/2
+
+    p = np.polyfit(times, a, deg=1)
+    slope = p[0]
+
+    mad_temp = a - median
+    meanAbsDev = np.mean(mad_temp)
+
+    q31 = q3 - q1
+
+    resBFR = sum(x < mean for x in a)/sum(x > mean for x in a)
+
+    skew = scipy.stats.skew(a)
+
+    kurtosis = scipy.stats.moment(a, moment=4)
+
 
 # print(repr(header1[0:24]))  # repr() prints the info into neat columns
 # binarytable = Table(binaryext)
@@ -63,19 +89,19 @@ with fits.open(filename, mode="readonly") as hdulist:
 #
 # print(x)
 
-# # Convert the time array to full BJD by adding the offset back in.
-bjds = times + bjdrefi + bjdreff
-
-plt.figure(figsize=(20, 9))
-
-# # Plot the time, uncorrected and corrected fluxes.
-plt.plot(bjds, sap_fluxes, '-k', label='SAP Flux')
-plt.plot(bjds, pdcsap_fluxes, '-b', label='PDCSAP Flux')
-
-plt.title('Kepler Light Curve')
-plt.legend()
-plt.xlabel('Time (days)')
-plt.ylabel('Flux (electrons/second)')
-plt.show()
+# # # Convert the time array to full BJD by adding the offset back in.
+# bjds = times + bjdrefi + bjdreff
+#
+# plt.figure(figsize=(20, 9))
+#
+# # # Plot the time, uncorrected and corrected fluxes.
+# plt.plot(bjds, sap_fluxes, '-k', label='SAP Flux')
+# plt.plot(bjds, pdcsap_fluxes, '-b', label='PDCSAP Flux')
+#
+# plt.title('Kepler Light Curve')
+# plt.legend()
+# plt.xlabel('Time (days)')
+# plt.ylabel('Flux (electrons/second)')
+# plt.show()
 
 
