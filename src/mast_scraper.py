65d5d055316f88277c0f7ee0cd9481d0da780f24
lcaprioli96 <93045436+lcaprioli17@users.noreply.github.com>
2022-07-01 17:39:36 +0200

    modicato scraping con spostamento e rinomina del file scaricato

--- src/mast_scraper.py
+++ src/mast_scraper.py
@@ -1,4 +1,6 @@
+import glob
 import math
+import os
 
 from astropy.io import fits
 from astropy.table import Table
@@ -14,33 +16,40 @@ k2 = pd.read_csv('../dataset/pp_dataset/k2ticids.csv', on_bad_lines='skip')
 
 ids = k2['tic_id']
 
-# for kepler scraping
+pathk2 = "A:/lightcurves/K2"
+pathkepler = "A:/lightcurves/Kepler"
+
+# # for kepler scraping
 # ids = ids.map(int)
 # ids = ids.map(str)
 
 for x in ids:
-    # kepler ids normalization for kepler scraping
+    # # kepler ids normalization for kepler scraping
     # if len(x) == 6:
-    #     target = "000" + x
+    #     target = "kplr000" + x
     #     print(target)
     # if len(x) == 7:
-    #     target = "00" + x
+    #     target = "kplr00" + x
     #     print(target)
     # if len(x) == 8:
-    #     target = "0" + x
+    #     target = "kplr0" + x
     #     print(target)
 
     print(x)
 
-    # K2 ids scraping with lightkurve package for K2 scraping
+    # # K2 ids scraping with lightkurve package for K2 scraping
     search_result = lk.search_lightcurve(x, author='K2')
 
     if not search_result:
         continue
 
-    # general scraping of light curves from MAST archive
+    # # for Kepler
+    # target = x
+    # for K2
     target = search_result[0]
 
+    # target depends on scraping type
+    # general scraping of light curves from MAST archive
     Obs = Observations.query_criteria(target_name=target.table['target_name'], obs_collection='K2')
     print("******************")
 
@@ -53,4 +62,7 @@ for x in ids:
 
     print(yourProd)
 
-    Observations.download_products(yourProd[0], mrp_only=False, cache=False, download_dir='A:/lightcurves')
+    # changing path
+    download = Observations.download_products(yourProd[0], mrp_only=False, cache=False, download_dir='A:/lightcurves')
+    curr_path = download[0].table['Local Path'].value
+    os.rename(curr_path[0], os.path.join(pathk2, x + '.fits'))
