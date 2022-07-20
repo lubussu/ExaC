6587a3d9e14ab1eae2c610a5a7c0a785928c2510
lubussu <bussuluana@gmail.com>
2022-07-20 20:29:35 +0200

    Revert "rimozione pl_mass causa errore"
    This reverts commit 9b50205ee851fcf12832a224ab8a1995f2e0db35.

--- src/pp_kepler.py
+++ src/pp_kepler.py
@@ -21,16 +21,22 @@ def pre_process():
                  'koi_trans_mod', 'koi_model_dof', 'koi_datalink_dvr', 'koi_datalink_dvs', 'koi_sparprov'],
         inplace=True)
 
+    # thresh = len(kepler) * .5
+    # kepler.dropna(thresh=thresh, axis=1, inplace=True)  # remove columns with less than 70% of not nan values
+
     nunique = kepler.nunique()  # series with number of unique value for each column
     cols_unique_drop = nunique[nunique == 1].index  # indexes of columns with value of nunique == 1
     kepler.drop(cols_unique_drop, axis=1, inplace=True)
 
-    kepler.drop_duplicates(subset='kepoi_name', keep='first', inplace=True)  # remove rows with duplicate value of kepoi_name column
+    kepler.drop_duplicates(subset='kepoi_name', keep='first')  # remove rows with duplicate value of kepoi_name column
 
     kepler = kepler[
         kepler.columns.drop(list(kepler.filter(regex='err')))]  # remove columns that contains err in attribute name
     kepler = kepler[
         kepler.columns.drop(list(kepler.filter(regex='lim')))]  # remove columns that contains lim in attribute name
+
+    # thresh = kepler.columns.size * .7
+    # kepler.dropna(thresh=thresh, axis=0, inplace=True)  # remove columns with less than 70% of not nan values
 
     # replace dispostion values in numeric values
     kepler['koi_disposition'].replace(
@@ -40,10 +46,29 @@ def pre_process():
     # feature extraction from lightcurves time series
     kepler = lcf.extract_features(kepler, 'kepid')
 
+    # # handling missing values:
+    # kepler2 = kepler.drop(columns=['kepoi_name', 'kepid'])
+    #
+    # kepler.dropna(axis=1, how='all', inplace=True)
+    #
+    # k = math.trunc(math.sqrt(len(kepler)))
+    # imputer = KNNImputer(n_neighbors=k, weights='uniform', metric='nan_euclidean')
+    # kepler_filled = pd.DataFrame(imputer.fit_transform(kepler2), columns=kepler2.columns)
+    #
+    # kepler_filled.insert(0, 'kepid', kepler['kepid'].values)
+    # kepler_filled.insert(0, 'kepoi_name', kepler['kepoi_name'].values)
+    # kepler_filled.to_csv('../dataset/pp_dataset/kepler_filled.csv')
+
     # remove attributes with a value of corr_m > 0.95 (see on the visual correlation matrix)
     kepler.drop(columns=['koi_fwm_sra', 'koi_fwm_sdec', 'koi_gmag', 'koi_rmag', 'koi_imag', 'koi_jmag',
                                 'koi_kmag', 'koi_sma', 'koi_ldm_coeff2', 'lc_max', 'lc_min',
                                 'lc_meanAbsDev', 'lc_q1'], inplace=True)
+
+    svolume = (4 / 3) * 3.14 * pow(kepler['koi_srad'], 3)
+    sdens = kepler['koi_smass'] / svolume
+    pdens = sdens * kepler['koi_srho']
+    pmass = pdens * (4 / 3) * 3.14 * pow(kepler['koi_prad'], 3)
+    kepler['pl_masse'] = pd.Series(pmass.values, index=kepler.index)
 
     kepler.to_csv('../dataset/pp_dataset/kepler.csv')
     return kepler
